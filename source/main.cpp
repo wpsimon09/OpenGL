9b6eef2ba7b770909c77bd1428ac6bf038cb35d1
Šimon <95319163+wpsimon09@users.noreply.github.com>
2023-07-20 20:28:22 +0200

    Revert "Advance opengl frame buffer mirror"

--- source/main.cpp
+++ source/main.cpp
@@ -9,8 +9,6 @@
 #include "camera.h"
 #include "Cube.h"
 #include "Model.h"
-#include "HelperFunctions.h";
-#include "Light.h";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
@@ -25,11 +23,8 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 //screen coordinates
 int SCR_WIDTH = 800;
 int SCR_HEIGHT = 600;
-glm::vec3 mirrorPosition(0.0f, 0.2f, 2.5f);
-
 // camera
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
-Camera mirrorCamera(mirrorPosition);
 float lastX = 800.0f / 2.0f;
 float lastY = 600.0f / 2.0f;
 bool firstMouse = true;
@@ -37,18 +32,6 @@ bool firstMouse = true;
 // timing
 float deltaTime = 0.0f;	// time between current frame and last frame
 float lastFrame = 0.0f;
-
-// light properties
-glm::vec3 direction = glm::vec3(0.2f, -1.0f, -0.6f);
-glm::vec3 ambient = glm::vec3(0.4f);
-glm::vec3 diffuse = COLOR_SUN;
-glm::vec3 specular = glm::vec3(1.0F);
-
-glm::vec3 diffusePoint = COLOR_SUN;
-
-float constant = 1.0f;
-float linear = 0.22f;
-float quadratic = 0.20f;
 
 
 //light possition
@@ -78,7 +61,7 @@ int main() {
 	//this code is to make context on the window current and to initialize glad
 	glfwMakeContextCurrent(window);
 	gladLoadGL();
-	//glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_TEST);
 	glfwSetCursorPosCallback(window, mouse_callback);
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	glfwSetScrollCallback(window, scroll_callback);
@@ -92,91 +75,15 @@ int main() {
 
 	stbi_set_flip_vertically_on_load(true);
 
-	Shader shader("VertexShader/DepthTestingVertex.glsl", "FragmentShader/DepthTestingFragment.glsl");
-	Shader screenShader("VertexShader/FrameBufferVertex.glsl", "FragmentShader/FrameBufferFragment.glsl");
+	Shader shader("VertexShader/vertexShader.glsl", "FragmentShader/fragmentShader.glsl");
+	Shader light("VertexShader/LightVertexShader.glsl", "FragmentShader/LightfragmentShader.glsl");
+	Model ourModel("Assets/Model/backpack/backpack.obj");
 
-	// cube VAO
-	unsigned int cubeVAO, cubeVBO;
-	glGenVertexArrays(1, &cubeVAO);
-	glGenBuffers(1, &cubeVBO);
-	glBindVertexArray(cubeVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	glBindVertexArray(0);
+	glViewport(0, 0, 800, 600);	
+	float textureOpacity = 0.0f;
 
-	// plane VAO
-	unsigned int planeVAO, planeVBO;
-	glGenVertexArrays(1, &planeVAO);
-	glGenBuffers(1, &planeVBO);
-	glBindVertexArray(planeVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), &planeVertices, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	glBindVertexArray(0);
-
-	//quad VAO
-	unsigned int quadVAO, quadVBO;
-	glGenVertexArrays(1, &quadVAO);
-	glGenBuffers(1, &quadVBO);
-	glBindVertexArray(quadVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
-	glBindVertexArray(0);
-
-	unsigned int cubeTexture = loadTexture("Assets/Textures/DepthTesting/metal.png");
-	unsigned int floorTexture = loadTexture("Assets/Textures/DepthTesting/marble.jpg");
-
-	shader.use();
-	shader.setInt("texture1", 0);
-
-	screenShader.use();
-	screenShader.setInt("screenTexture", 0);
-
-	// frame buffer
-	unsigned int fbo;
-	glGenFramebuffers(1, &fbo);
-	glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-	
-	unsigned int fboTexture;
-	glGenTextures(1, &fboTexture);
-	glBindTexture(GL_TEXTURE_2D, fboTexture);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, fboTexture, 0);
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	//render buffer object for depth and stencil values
-	unsigned int rbo;
-	glGenRenderbuffers(1, &rbo);
-	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SCR_WIDTH, SCR_HEIGHT);
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
-	glBindRenderbuffer(GL_RENDERBUFFER, 0);
-
-
-	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE)
-	{
-		std::cout << "-------------FRAME BUFFER BOUND SUCCESSFULLY-----------------\n";
-	}
-	else
-		std::cout << "ERROR:BUFFER:FRAME\n Frame buffer not bound successfully \n";
-			
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
+	const float radius = 10.0f;
+	bool isFlashOn = true;
 	while (!glfwWindowShouldClose(window))
 	{
 		// per-frame time logic
@@ -185,130 +92,41 @@ int main() {
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
 
-
 		// input
 		// -----
 		processInput(window);
 
-		glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-		glEnable(GL_DEPTH_TEST);
-		
-		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-
-		//---------------------------------
-		// RENDER SCENE IN OUR FRAME BUFFER
-		//---------------------------------
-		shader.use();
-		glm::mat4 model = glm::mat4(1.0f);
-
-		camera.ProcessMouseMovement(0, 0, false);
-		glm::mat4 view = mirrorCamera.GetViewMatrix();
-		glm::mat4 projection = glm::perspective(glm::radians(80.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-		shader.setMat4("view", view);
-		shader.setMat4("projection", projection);
-
-		//----------------------
-		// DRAW CUBE 1
-		//----------------------
-		glBindVertexArray(cubeVAO);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, cubeTexture);
-		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
-		shader.setMat4("model", model);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-
-		//----------------------
-		// DRAW CUBE 2
-		//----------------------
-		model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
-		shader.setMat4("model", model);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-
-		//----------------------
-		// DRAW CUBE THAT REPRESENTS PLAYER
-		//----------------------
-		model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(camera.Position));
-		model = glm::scale(model, glm::vec3(0.1f));
-		shader.setMat4("model", model);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-
-		//----------------------
-		// DRAW PLANE AS A FLOOR
-		//----------------------
-		glBindVertexArray(planeVAO);
-		glBindTexture(GL_TEXTURE_2D, floorTexture);
-		shader.setMat4("model", glm::mat4(1.0f));
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-
-		glBindVertexArray(0);
-
-		//---------------------------------------------
-		// RENDER THE FRAME BUFFER CONTAINING OUR SCENE 
-		//---------------------------------------------
-	
-		glBindFramebuffer(GL_FRAMEBUFFER, 0); // back to default
-		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		// render
+		// ------
+		glClearColor(1.0f, 0.05f, 0.05f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		//--------------------------------------
-		// DRAW NORMAL SCENE default frame buffer
-		//--------------------------------------
+		// don't forget to enable shader before setting uniforms
 		shader.use();
-		model = glm::mat4(1.0f);
-		view = camera.GetViewMatrix();
-		projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-		shader.setMat4("view", view);
+
+		//glm::vec3 direction = glm::vec3(0.0f,1.0f,0.0f);
+		//glm::vec3 ambient = glm::vec3(0.3f);
+		//glm::vec3 diffuse = glm::vec3(1.0f);
+		//glm::vec3 specular = glm::vec3(1.0F);
+		//
+		//shader.setVec3("dirLight.direction", direction);
+		//shader.setVec3("dirLight.ambient", ambient);
+		//shader.setVec3("dirLight.diffuse", diffuse);
+		//shader.setVec3("dirLight.specular", specular);
+
+		// view/projection transformations
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		glm::mat4 view = camera.GetViewMatrix();
 		shader.setMat4("projection", projection);
+		shader.setMat4("view", view);
 
-		//----------------------
-		// DRAW CUBE 1
-		//----------------------
-		glBindVertexArray(cubeVAO);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, cubeTexture);
-		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+		// render the loaded model
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
+		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
 		shader.setMat4("model", model);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		ourModel.Draw(shader);
 
-		//----------------------
-		// DRAW CUBE 2
-		//----------------------
-		model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
-		shader.setMat4("model", model);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-
-		
-
-		
-		//----------------------
-		// DRAW PLANE AS A FLOOR
-		//----------------------
-		glBindVertexArray(planeVAO);
-		glBindTexture(GL_TEXTURE_2D, floorTexture);
-		shader.setMat4("model", glm::mat4(1.0f));
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-
-		glBindVertexArray(0);
-
-		//---------------------
-		// DRAW THE MIRROR in the frame buffer
-		//---------------------
-		screenShader.use();
-
-		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 2.5));
-		model = glm::scale(model, glm::vec3(5.0f));
-		
-		screenShader.setMat4("model", model);
-		screenShader.setMat4("view", view);
-		screenShader.setMat4("projection", projection);
-		
-		glBindVertexArray(quadVAO);
-		glBindTexture(GL_TEXTURE_2D, fboTexture);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
