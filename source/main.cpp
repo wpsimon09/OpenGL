59ad88143d44a28472622aa689c1c01a0a4ac9ab
Šimon <simon.potocnak03@gmail.com>
2023-07-30 10:54:34 +0200

    main branch now contains skybox with glass model

--- source/main.cpp
+++ source/main.cpp
@@ -22,6 +22,7 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 
+//screen coordinates
 int SCR_WIDTH = 1000;
 int SCR_HEIGHT = 800;
 // camera
@@ -74,7 +75,7 @@ int main() {
 	//this code is to make context on the window current and to initialize glad
 	glfwMakeContextCurrent(window);
 	gladLoadGL();
-	//glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_TEST);
 	glfwSetCursorPosCallback(window, mouse_callback);
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	glfwSetScrollCallback(window, scroll_callback);
@@ -129,41 +130,6 @@ int main() {
 	skyboxShader.use();
 	skyboxShader.setInt("skybox", 0);
 
-
-	// frame buffer
-	unsigned int fbo;
-	glGenFramebuffers(1, &fbo);
-	glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-	
-	unsigned int fboTexture;
-	glGenTextures(1, &fboTexture);
-	glBindTexture(GL_TEXTURE_2D, fboTexture);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, fboTexture, 0);
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	//render buffer object for depth and stencil values
-	unsigned int rbo;
-	glGenRenderbuffers(1, &rbo);
-	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, SCR_WIDTH, SCR_HEIGHT);
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
-	glBindRenderbuffer(GL_RENDERBUFFER, 0);
-
-
-	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE)
-	{
-		std::cout << "-------------FRAME BUFFER BOUND SUCCESSFULLY-----------------\n";
-	}
-	else
-		std::cout << "ERROR:BUFFER:FRAME\n Frame buffer not bound successfully \n";
-			
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
 	while (!glfwWindowShouldClose(window))
 	{
 		// per-frame time logic
@@ -171,6 +137,7 @@ int main() {
 		float currentFrame = static_cast<float>(glfwGetTime());
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
+
 		glClearColor(0.701f, 0.501f, 0.501f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -188,9 +155,12 @@ int main() {
 		shader.setMat4("projection", projection);
 		shader.setMat4("model", model);
 
-
 		shader.setVec3("cameraPos", camera.Position);
 
+		//----------------------
+		// DRAW CUBE 1
+		//----------------------
+		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_CUBE_MAP, cubeTexture);
 
 		backpack.Draw(shader);
@@ -202,20 +172,17 @@ int main() {
 
 		skyboxShader.use();
 		view = glm::mat4(glm::mat3(camera.GetViewMatrix()));
-		
+
 		skyboxShader.setMat4("projection", projection);
 		skyboxShader.setMat4("view", view);
 
 		glBindVertexArray(skyBoxVAO);
 		glBindTexture(GL_TEXTURE_CUBE_MAP, skyBox);
-		glBindTexture(GL_TEXTURE_2D, cubeTexture);
-		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
-		shader.setMat4("model", model);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 
-	
-		glBindVertexArray(0);
+		glDepthFunc(GL_LESS);
 
+		glBindVertexArray(0);
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
 		glfwSwapBuffers(window);
@@ -234,7 +201,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow *window)
+void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
@@ -262,7 +229,7 @@ float opacityOfTexture(GLFWwindow* window, Shader shader) {
 }
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
-	
+
 	if (firstMouse) // initially set to true
 	{
 		lastX = xpos;
@@ -280,7 +247,7 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
 
 }
 
-void scroll_callback(GLFWwindow * window, double xoffset, double yoffset)
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
 	camera.ProcessMouseScroll(static_cast<float>(yoffset));
 }
