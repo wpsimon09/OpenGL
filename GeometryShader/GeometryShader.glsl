40304f7851fdfa062134159745c32e39d797c2de
Šimon <simon.potocnak03@gmail.com>
2023-07-31 19:55:52 +0200

    generate house for each of the point

--- GeometryShader/GeometryShader.glsl
+++ GeometryShader/GeometryShader.glsl
@@ -1,10 +1,24 @@
 #version 330 core
 layout (points) in;
-layout (points, max_vertices = 1) out;
-
-void main() {
-	gl_Position = gl_in[0].gl_Position + 2.0;
-	gl_PointSize = 20;
+layout (triangle_strip, max_vertices = 5) out;
+void build_house(vec4 position)
+{
+	gl_Position = position + vec4(-0.2, -0.2, 0.0, 0.0); // 1:bottom-left
+	EmitVertex();
+	
+	gl_Position = position + vec4( 0.2, -0.2, 0.0, 0.0); // 2:bottom-right
+	EmitVertex();
+	
+	gl_Position = position + vec4(-0.2, 0.2, 0.0, 0.0); // 3:top-left
+	EmitVertex();
+	
+	gl_Position = position + vec4( 0.2, 0.2, 0.0, 0.0); // 4:top-right
+	EmitVertex();
+	
+	gl_Position = position + vec4( 0.0, 0.4, 0.0, 0.0); // 5:top
 	EmitVertex();
 	EndPrimitive();
-}	
+}
+void main() {
+	build_house(gl_in[0].gl_Position);
+}
