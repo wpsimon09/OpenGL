ec76d38314f6ad51a07ca71cb90f8ed8e9de4ac7
Šimon <simon.potocnak03@gmail.com>
2023-07-31 19:36:03 +0200

    working geometry shader

--- GeometryShader/GeometryShader.glsl
+++ GeometryShader/GeometryShader.glsl
@@ -3,8 +3,8 @@ layout (points) in;
 layout (points, max_vertices = 1) out;
 
 void main() {
-	gl_Position = gl_in[0].gl_Position;
-	gl_PointSize = 9.2;
+	gl_Position = gl_in[0].gl_Position + 2.0;
+	gl_PointSize = 20;
 	EmitVertex();
 	EndPrimitive();
-}
+}	
