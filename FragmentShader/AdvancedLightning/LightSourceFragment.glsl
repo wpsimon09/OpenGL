098f3b1fe1c165a6fde20af5bd85e91755f9b9b3
Šimon <simon.potocnak03@gmail.com>
2023-09-13 10:35:45 +0200

    render bright sposts to the different color buffer targets so that we can calculate bloom

--- FragmentShader/AdvancedLightning/LightSourceFragment.glsl
+++ FragmentShader/AdvancedLightning/LightSourceFragment.glsl
@@ -1,12 +1,13 @@
 #version 330 core
 
+layout(location = 0) out vec4 FragColor;
+layout(location = 1) out vec4 BrightColor;
+
 uniform sampler2D lightTexture;
 
 uniform vec3 lightColor;
 
 in vec2 TextureCoords;
-
-out vec4 FragmentColor;
 
 void main() {
 
@@ -14,5 +15,11 @@ void main() {
 	if(texColor.a < 0.1)
 		discard;
 
-	FragmentColor = vec4(lightColor, 1.0f);
+	FragColor = vec4(lightColor, 1.0f);
+
+	float brightness = dot(FragColor.rgb, vec3(0.2126, 0.7152, 0.0722));
+		if(brightness > 1.0)
+			FragColor= vec4(FragColor.rgb, 1.0);
+		else
+			FragColor = vec4(0.0, 0.0, 0.0, 1.0);
 }
